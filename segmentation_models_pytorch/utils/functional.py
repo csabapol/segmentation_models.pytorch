73478fa4d1c3d80609e3c5f4dcf1b2a2f1f7e1aa
Csaba Rohrer <csaba.rohrer@charite.de>
2021-03-09 19:20:28 +0100

    weighted dice loss

--- segmentation_models_pytorch/utils/functional.py
+++ segmentation_models_pytorch/utils/functional.py
@@ -5,8 +5,15 @@ def _take_channels(*xs, ignore_channels=None):
     if ignore_channels is None:
         return xs
     else:
-        channels = [channel for channel in range(xs[0].shape[1]) if channel not in ignore_channels]
-        xs = [torch.index_select(x, dim=1, index=torch.tensor(channels).to(x.device)) for x in xs]
+        channels = [
+            channel
+            for channel in range(xs[0].shape[1])
+            if channel not in ignore_channels
+        ]
+        xs = [
+            torch.index_select(x, dim=1, index=torch.tensor(channels).to(x.device))
+            for x in xs
+        ]
         return xs
 
 
@@ -39,6 +46,58 @@ def iou(pr, gt, eps=1e-7, threshold=None, ignore_channels=None):
 jaccard = iou
 
 
+def weighted_f_score(
+    pr, gt, class_weights, beta=1, eps=1e-7, threshold=None, ignore_channels=None
+):
+    """Calculate F-score between ground truth and prediction
+    Args:
+        pr (torch.Tensor): predicted tensor
+        gt (torch.Tensor):  ground truth tensor
+        beta (float): positive constant
+        eps (float): epsilon to avoid zero division
+        threshold: threshold for outputs binarization
+        class_weights: weights for each class/channels
+    Returns:
+        float: F score
+    """
+
+    pr = _threshold(pr, threshold=threshold)
+    pr, gt = _take_channels(pr, gt, ignore_channels=ignore_channels)
+
+    scores = get_channel_scores(pr, gt, beta, eps, class_weights)
+
+    return scores
+
+
+def get_channel_scores(pr, gt, beta, eps, class_weights):
+    scores = []
+    num_channels = gt.shape[1]
+    for channel in range(num_channels):
+        pr_ch = pr[:, channel, :, :]
+        gt_ch = gt[:, channel, :, :]
+
+        tp = torch.sum(gt_ch * pr_ch)
+        fp = torch.sum(pr_ch) - tp
+        fn = torch.sum(gt_ch) - tp
+
+        score = ((1 + beta ** 2) * tp + eps) / (
+            (1 + beta ** 2) * tp + beta ** 2 * fn + fp + eps
+        )
+
+        scores.append(score.cpu().detach().numpy())
+
+    avg_loss = average_loss(scores, class_weights)
+    avg_loss = torch.tensor(avg_loss, device=gt.device)
+
+    return avg_loss
+
+
+def average_loss(scores, class_weights):
+    # print(scores, class_weights)
+    x = scores * np.array(class_weights)
+    return np.mean(x)
+
+
 def f_score(pr, gt, beta=1, eps=1e-7, threshold=None, ignore_channels=None):
     """Calculate F-score between ground truth and prediction
     Args:
@@ -58,8 +117,9 @@ def f_score(pr, gt, beta=1, eps=1e-7, threshold=None, ignore_channels=None):
     fp = torch.sum(pr) - tp
     fn = torch.sum(gt) - tp
 
-    score = ((1 + beta ** 2) * tp + eps) \
-            / ((1 + beta ** 2) * tp + beta ** 2 * fn + fp + eps)
+    score = ((1 + beta ** 2) * tp + eps) / (
+        (1 + beta ** 2) * tp + beta ** 2 * fn + fp + eps
+    )
 
     return score
 
